104f6a80a5b85e1cb2a7a5826b0de0a7fbf549bd
Yash Aryan <yash.aryan1@learner.manipal.edu>
2020-10-29 00:00:30 +0530

    backbone code

---
+++ server.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const { check, validationResult } = require('express-validator');
+
+// check for environment offered port, or switch to default of 3000
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+//global variable to get time once the server starts listening
+const serverstartTime = new Date();
+
+//function to determine if the server should send a server error message or continue processing the request
+const serverActiveOrNot = () => {
+  //Parthiv's code goes here
+  return continueOrNot;
+  //returns a boolean. True if the code is in the active 4 hours period, and false if the code is in server off period (2 hours)
+};
+
+//function to randomly deny login attempt based on generated random number's divisibility.
+const logInOrNot = () => {
+  //Dhruv's code goes here
+  return continueOrNot;
+  //returns a boolean. True if the odds are in favor of logging in P(E) = 0.4 and false if the odds are against letting the user log in P(E) = 0.6
+};
+
+// authenticated status checking function
+const checkAuthenticated = () => {
+  // code goes here. YASH
+};
+
+// the main login feature
+app.get(
+  '/login',
+  [
+    checkAuthenticated(),
+    [
+      check('email', 'Please include a valid email').isEmail(),
+      check('password', 'Please enter a valid password').isLength({ min: 6 })
+    ]
+  ],
+  async (req, res) => {
+    //checking if the user input is correct, user input sanitization
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.log(errors);
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    //enclosing the code in a try-catch block
+    try {
+      // getting the server status from the functions
+      const serverStatus = await serverActiveOrNot();
+      const loginStatus = await serverStatus();
+      //check if the server is up or not. If it is, accept the request and forward it to other functions
+      if (serverStatus) {
+        if (loginStatus) {
+          // if the loginstatus is true, send the JWT Token to the user for authentication
+          // Yash's code goes here
+        } else {
+          // if the loginstatus was false, send a server error message
+          res
+            .status(504)
+            .json({ msg: 'Our primary servers took too long to respond.' });
+        }
+      }
+      // if the server is down, send server unavailable error code.
+      else {
+        res.status(503).json({ msg: 'Server Unavailable' });
+      }
+    } catch (err) {
+      // send an error if there was actually an error in the server
+      res.status(500).json({ msg: 'There was an error' });
+    }
+  }
+);
+
+
+//people can use this route to check if the server is active or not
+app.get('/', (req, res) => {
+      const serverStatus = await serverActiveOrNot();
+      if(serverStatus) {
+            res.status(200).json({msg: "The server is online"});
+      }
+      else {
+            res.status(500).json({ msg: 'Server error' });
+      }
+});
+
+
+//people can use this route to get the server error report. This route can be found if people inspect the source of the original website
+
+//listen to the requests on port ${PORT}
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
