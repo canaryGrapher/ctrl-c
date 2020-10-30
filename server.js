2b04565f3a95d1d4ce1ce3a87a3ed37888be1409
Yash Aryan <yash.aryan1@learner.manipal.edu>
2020-10-30 10:23:08 +0530

    initialized react

--- server.js
+++ server.js
@@ -1,18 +1,38 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
-const username
+// authenticated status checking module
+const auth = require('./authentication/auth');
+const rn = require('random-number');
+const options = {
+  min: 1,
+  max: 1000,
+  integer: true
+};
 
 // check for environment offered port, or switch to default of 3000
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-//global variable to get time once the server starts listening
-const serverstartTime = new Date();
+//setting up middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//off time container array
+let continueOrNot;
+const serverOffTime = [21, 22, 03, 04, 09, 10, 11, 12, 17, 18]; //assuming the server goes up at 1700 hours
 
 //function to determine if the server should send a server error message or continue processing the request
 const serverActiveOrNot = () => {
   //Parthiv's code goes here
+  const timeNow = new Date();
+  if(!serverOffTime.includes(timeNow.getHours())) {
+    continueOrNot = true;
+  }
+  else if(serverOffTime.includes(timeNow.getHours())) {
+    continueOrNot = false;
+  }
   return continueOrNot;
   //returns a boolean. True if the code is in the active 4 hours period, and false if the code is in server off period (2 hours)
 };
@@ -20,35 +40,28 @@ const serverActiveOrNot = () => {
 //function to randomly deny login attempt based on generated random number's divisibility.
 const logInOrNot = () => {
   //Dhruv's code goes here
+  //returns a boolean. True if the odds are in favor of logging in P(E) = 0.7 and false if the odds are against letting the user log in P(E) = 0.3
+  const continueOrNot = rn(options)%3 != 0 ? true : false;
   return continueOrNot;
-  //returns a boolean. True if the odds are in favor of logging in P(E) = 0.4 and false if the odds are against letting the user log in P(E) = 0.6
-};
-
-// authenticated status checking function
-const checkAuthenticated = () => {
-  // code goes here. YASH
+  
 };
 
 //people can use this route to check if the server is active or not
-app.get('/', (req, res) => {
-      const serverStatus = await serverActiveOrNot();
-      if(serverStatus) {
-            res.status(200).json({msg: "The server is online"});
-      }
-      else {
-            res.status(500).json({ msg: 'Server error' });
-      }
+app.get('/', async (req, res) => {
+  const serverStatus = serverActiveOrNot();
+  if (serverStatus) {
+    res.status(200).json({ msg: 'The server is online' });
+  } else {
+    res.status(500).json({ msg: 'Server error' });
+  }
 });
 
 // the main login feature
 app.post(
   '/login',
   [
-    checkAuthenticated(),
-    [
-      check('email', 'Please include a valid email').isEmail(),
-      check('password', 'Please enter a valid password').isLength({ min: 6 })
-    ]
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a valid password').isLength({ min: 6 })
   ],
   async (req, res) => {
     //checking if the user input is correct, user input sanitization
@@ -61,20 +74,38 @@ app.post(
     //enclosing the code in a try-catch block
     try {
       // getting the server status from the functions
-      const serverStatus = await serverActiveOrNot();
-      const loginStatus = await serverStatus();
+      const serverStatus = serverActiveOrNot();
+      const loginStatus = logInOrNot();
       //check if the server is up or not. If it is, accept the request and forward it to other functions
       if (serverStatus) {
         if (loginStatus) {
-            //deconstruct the body of the request
-            const email, password = req.body
-            if(email === "admin@wearemist.in" && password === "thispassworddoesnotexist") {}
-            // if the loginstatus is true, send the JWT Token to the user for authentication
-            // Yash's code goes here
-            //failed login
-            else {
-                  res.status(401).json({msg: "Login failed. Invalid credentials"})
-            } 
+          //deconstructing the body of the request
+          const { email, password } = req.body;
+          if (
+            email === config.get('email') &&
+            password === config.get('password')
+          ) {
+            const payload = {
+              user: {
+                id: config.get('email')
+              }
+            };
+            jwt.sign(
+              payload,
+              config.get('jwtSecret'),
+              { expiresIn: 360000 },
+              (err, token) => {
+                if (err) throw err;
+                res.json({ token });
+              }
+            );
+          }
+          // if the loginstatus is true, send the JWT Token to the user for authentication
+          // Yash's code goes here
+          //failed login
+          else {
+            res.status(401).json({ msg: 'Login failed. Invalid credentials' });
+          }
         } else {
           // if the loginstatus was false, send a server error message
           res
@@ -93,15 +124,23 @@ app.post(
   }
 );
 
-
 //people can use this route to get the server error report. This route can be found if people inspect the source of the original website
-app.get("/insiderinfoprovider", (req, res) => {
-      res.status(200).send("Send a long paragraph explaining how shitty the server is and how it affects their development")
-})
+app.get('/insiderinfoprovider', (req, res) => {
+  res
+    .status(200)
+    .send(
+      'Send a long paragraph explaining how shitty the server is and how it affects their development'
+    );
+});
 
-app.get("*", (req, res) => {
-      res.status(404).json({msg: "Whatever you were trying, it's not gonna work"})
-})
+//this is the data the contestants have to extract. Populate it with pseudo-bank details. Getting this would earn them bonus points.
+app.get('/getTheExtractedData', auth, (req, res) => {});
+
+app.get('*', (req, res) => {
+  res
+    .status(404)
+    .json({ msg: "Whatever you were trying, it's not gonna work" });
+});
 
 //listen to the requests on port ${PORT}
 app.listen(PORT, () => {
